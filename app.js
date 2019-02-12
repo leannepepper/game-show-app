1850412ac5312f14aed7cbdfb4f9e7f01246f6e1
LeannePepper <leanne.werner.lw@gmail.com>
2019-02-12 14:29:18 -0800

    added checkletter function

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
 const qwerty = document.querySelector('#qwerty'); 
-const phrase = document.querySelector('#phrase'); 
+const phraseUl = document.querySelector('#phrase ul'); 
+const resetButton = document.querySelector('.btn__reset');
+const title = document.querySelector('.title');
+// const letters = document.querySelectorAll('.letter');
 let missed = 0; 
 
-const resetButton = document.querySelector('.btn__reset'); 
-
+// function to remove overlay on Start
 resetButton.addEventListener('click', function (){
     document.querySelector('#overlay').style.display = 'none'; 
 })
 
-const phraseArray = ['when pigs fly','once in a blue moon','piece of cake','kill two birds with one stone','best of both worlds'];
-
+const phrases = [
+    'when pigs fly',
+    'once in a blue moon',
+    'piece of cake',
+    'kill two birds with one stone',
+    'best of both worlds'
+]
+// get a random phrase from the array, then split letters into another array 
 function getRandomPhraseAsArray(arr) {
-    return arr[Math.floor(Math.random() * phraseArray.length)].split(''); 
+    return arr[Math.floor(Math.random() * phrases.length)].split(''); 
 }
-console.log(getRandomPhraseAsArray(phraseArray)); 
+// display the random phrase letters array to the document 
+const addPhraseToDisplay = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement('li'); 
+            li.textContent = arr[i]; 
+            phraseUl.appendChild(li); 
+            if(arr[i] === " ") {
+                li.className = "space"; 
+            } else {
+                li.className = "letter"; 
+            }
+    }
+} 
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
 
+// checkLetter function 
+// const letterButtons = document.querySelectorAll('.keyrow button'); 
+
+function checkLetter (letterButtons) {
+    let check = null; 
+
+    const letters = document.querySelectorAll('.letter');
+    for(let i=0; i < letters.length; i++) {
+        if(letterButtons.textContent === letters[i].textContent.toLowerCase()) {
+            letters[i].classList.add("show");
+            check = true;  
+        }
+    }
+    return check;
+}
+
+qwerty.addEventListener('click', (e) => {
+    let letterFound = checkLetter(e.target); 
+    
+    if(e.target.tagName === 'BUTTON'){
+        e.target.classList.add('chosen'); 
+        e.target.disabled = true; 
+
+    }
+   
+})
