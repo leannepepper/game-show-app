64e40bde3f479df9ffd9a303dadf1c5620c93757
LeannePepper <leanne.werner.lw@gmail.com>
2019-02-15 08:11:24 -0800

    Add reset functions to the game

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const resetButton = document.querySelector('.btn__reset');
 const title = document.querySelector('.title');
 const tries = document.querySelectorAll('.tries');
 const scoreBoard = document.querySelector('#scoreboard ol');
+const buttons = document.getElementsByTagName('button');
 const overlay = document.querySelector('#overlay');
 let missed = 0; 
 
@@ -39,8 +40,7 @@ const addPhraseToDisplay = (arr) => {
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
-// checkLetter function 
-// const letterButtons = document.querySelectorAll('.keyrow button'); 
+// checkLetter function to add classes to correctly clicked letters 
 
 function checkLetter (letterButtons) {
     let check = null; 
@@ -55,30 +55,34 @@ function checkLetter (letterButtons) {
     }
     return check;
 }
+
 function checkWin() {
     if(document.querySelectorAll('.letter').length == document.querySelectorAll('.show').length) {
         overlay.style.display = "block";
         overlay.innerHTML ="YOU WIN";
         overlay.style.fontSize = "40px";
         overlay.style.paddingTop = "100px";
-        overlay.classList.remove('start'); 
+        overlay.classList.remove('start', 'lose'); 
         overlay.classList.add('win');
+
+        resetGame ();
     }
     else if(missed >= 5){
         overlay.style.display = "block";
         overlay.innerHTML ="GAME OVER";
         overlay.style.fontSize = "40px";
         overlay.style.paddingTop = "100px";
-        overlay.classList.remove('start'); 
+        overlay.classList.remove('start', 'win'); 
         overlay.classList.add('lose');
 
+        resetGame ();
      }
-
 }
-
+// add classes to buttons that are clicked, remove heart if click is incorrect 
 qwerty.addEventListener('click', (e) => {
     let letterFound = checkLetter(e.target); 
-    if(e.target.tagName === 'BUTTON'){
+    
+    if(e.target.tagName === 'BUTTON') {
         e.target.classList.add('chosen'); 
         e.target.disabled = true; 
 
@@ -86,10 +90,53 @@ qwerty.addEventListener('click', (e) => {
             missed += 1; 
             console.log(missed);
             tries[missed-1].style.display = "none"; 
-            }
-        }   
-        checkWin();     
+        }
+    }   
+    checkWin();     
 })
+
+// The following functions are to reset the game. 
+// This inclueds resetting the random phrase, the hearts, and the keyboard functions. 
+
+// remove the previous phrase and fetch another random phrase 
+function removeLetters () {
+    phraseUl.innerHTML = " ";
+    addPhraseToDisplay(getRandomPhraseAsArray(phrases)); 
+}
+// replace the "tries" for reseting the game 
+function heartReplace () {
+    let newHeart = Array.from(tries);
+      newHeart.forEach(function(hrt) {
+        hrt.style.display ="inline-block"
+    });
+    missed= 0;
+}
+// reset the key to restore normal function
+  function keysReplace () {
+	for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("chosen");
+        buttons[i].disabled = false;
+    }
+
+};
+
+// create the reset button, remove the overlay and call resetting functions 
+function resetGame () {
+  const startAgain = document.createElement('button'); 
+  startAgain.classList.add('startAgain'); 
+  startAgain.innerHTML = "Restart Game";
+
+  overlay.appendChild(startAgain); 
+  
+    startAgain.addEventListener("click", function (){
+        overlay.style.display = "none";
+         
+        removeLetters();
+        heartReplace();    
+        keysReplace ();    
+    })
+
+}
 
 
 
